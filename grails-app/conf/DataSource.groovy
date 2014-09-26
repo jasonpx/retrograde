98b3116845863ea0e00bbcad90cbb627f063e1c8
Jason Priest <jpriest@amplify.com>
2014-09-26 14:43:01 -0400

    Setting app to work with Heroku

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -44,6 +44,19 @@ environments {
         }
     }
     production {
+      dataSource {
+          dbCreate = "update"
+          driverClassName = "org.postgresql.Driver"
+          dialect = org.hibernate.dialect.PostgreSQLDialect
+
+          uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+
+          url = "jdbc:postgresql://"+uri.host+uri.path
+          username = uri.userInfo.split(":")[0]
+          password = uri.userInfo.split(":")[1]
+      }
+    }
+    production2 {
         dataSource {
             //dbCreate = "create-drop"
             driverClassName = "org.postgresql.Driver"
